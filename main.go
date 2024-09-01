35818e53deac0a09bd34e7ad41510b6558aa93d9
Ernesto De Crecchio <decrecchio.ernesto@gmail.com>
2024-09-01 22:39:08 +0200

    Initialize DB with modernc sqlite driver

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/ITASerus/Go-Event-REST-API/db"
 	"github.com/ITASerus/Go-Event-REST-API/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
