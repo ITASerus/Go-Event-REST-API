a974b161bd4c0f1ed4a3554bc0e7cf2537a76684
Ernesto De Crecchio <decrecchio.ernesto@gmail.com>
2024-09-07 14:32:48 +0200

    Add login API

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/ITASerus/Go-Event-REST-API/db"
 	"github.com/ITASerus/Go-Event-REST-API/utils"
 )
@@ -38,4 +40,22 @@ func (u User) Save() error {
 	u.ID = userId
 
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
 }
