99b7f9b799a7dc844db94d004731e868f270dc54
Ernesto De Crecchio <decrecchio.ernesto@gmail.com>
2024-09-08 21:50:44 +0200

    JWT Token management

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/ITASerus/Go-Event-REST-API/models"
+	"github.com/ITASerus/Go-Event-REST-API/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -41,5 +42,11 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful", "token": token})
 }
